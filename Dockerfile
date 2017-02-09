595fb6ad96cbf2c2bc3d9e6855a1b039a7152c82
Christian Kroemer <christian.kroemer@comsysto.com>
2017-02-09 08:43:33 +0100

    a few more cleanups

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ FROM anapsix/alpine-java:latest
 ####
 
 WORKDIR /opt/petclinic
-ADD target/spring-petclinic-1.4.2.jar .
-CMD java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=14441,server=y,suspend=n -jar spring-petclinic-1.4.2.jar
+ADD build/petclinic-example/target/spring-petclinic-1.5.1.jar .
+CMD java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=14441,server=y,suspend=n -jar spring-petclinic-1.5.1.jar
--- build-petclinic-db-docker-compose.sh
+++ build-petclinic-db-docker-compose.sh
@@ -19,7 +19,7 @@ docker-compose stop
 # unfortunately, we cannot simply commit the result as the content of volumes is ignored - therefore we use cp
 docker cp `docker-compose --file docker-compose-mysql-image-builder.yml ps -q mysql`:/var/lib/mysql ${TMP_DATA_DIR}
 # the image is tagged with the MD5 checksum of the migration files, so we may skip the whole process if migration files haven't changed
-docker build --build-arg data_directory=${TMP_DATA_DIR} --tag ${DB_IMAGE_NAME}:MD5_$(<build/distributions/spring-petclinic-1.4.2-migration.zip.MD5) --file Dockerfile-db-compose .
+docker build --build-arg data_directory=${TMP_DATA_DIR} --tag ${DB_IMAGE_NAME}:MD5_$(<build/distributions/spring-petclinic-1.4.2-migration.zip.MD5) --tag ${DB_IMAGE_NAME}:latest --file Dockerfile-db-compose .
 
 # clean up everything
 docker-compose --file docker-compose-mysql-image-builder.yml rm -f
